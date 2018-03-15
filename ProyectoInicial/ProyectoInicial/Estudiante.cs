070f8540c739d6d2b3d9f3ba978dadd4c8def188
PROTAB <PROTAB@DESKTOP-QIC16QR>
2018-03-15 18:50:59 -0500

    Creacion de la clase Estudiante

---
+++ ProyectoInicial/ProyectoInicial/Estudiante.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoInicial
+{
+    class Estudiante
+    {
+        private string name;
+        private int nota1;
+        private int nota2;
+        private int nota3;
+
+        public Estudiante(int n1, int n2, int n3) {
+            Nota1 = n1;
+            Nota2 = n2;
+            Nota3 = n3;
+        }
+        /// <summary>
+        /// Obtiene el promedio de las 3 notas, retornando un double
+        /// </summary>
+        /// <returns></returns>
+        public double getAverage()
+        {
+            return (Nota1 + Nota2 + Nota3) / 3;
+        }
+        public int Nota1
+        {
+            get
+            {
+                return nota1;
+            }
+
+            set
+            {
+                nota1 = value;
+            }
+        }
+
+        public int Nota2
+        {
+            get
+            {
+                return nota2;
+            }
+
+            set
+            {
+                nota2 = value;
+            }
+        }
+
+        public int Nota3
+        {
+            get
+            {
+                return nota3;
+            }
+
+            set
+            {
+                nota3 = value;
+            }
+        }
+    }
+}
--- ProyectoInicial/ProyectoInicial/Program.cs
+++ ProyectoInicial/ProyectoInicial/Program.cs
@@ -10,7 +10,9 @@ namespace ProyectoInicial
     {
         static void Main(string[] args)
         {
-
+            Estudiante estudiante1 = new Estudiante(11,12,13);
+            Console.WriteLine("Nota del estudiante 1: " + estudiante1.getAverage());
+            Console.ReadLine();
         }
     }
 }
